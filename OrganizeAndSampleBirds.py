60f97f5d8f637a004ff9f30b50b8a21c0e0fe290
billydexter <driggs.nathan@gmail.com>
2021-04-09 11:54:19 -0600

    First commit

---
+++ OrganizeAndSampleBirds.py
@@ -0,0 +1,20 @@
+class Bird:
+    def __init__(self, name, filename):
+        self.name = name
+        self.filename = filename
+
+def readPNGs():
+    birds = []
+    for i in ["robin", "sparrow", "starling", "chickadee", "dove", "finch", "flicker", "magpie"]:
+        for j in range(1, 11):
+            filename = "./Birdsong images/" + i + "/" + i + "_" + str(j)
+            bird = Bird(filename, i)
+            birds.append(birds)
+    return birds
+
+def divideBirds(proportion):
+    birds = readPNGs()
+    index = int(len(birds) * proportion)
+    trainingData = birds[:index]
+    testingData = birds[index:]
+    return trainingData, testingData
